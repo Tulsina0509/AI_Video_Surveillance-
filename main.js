368ff952119ad17ea0c7a67fcbca08e3895cede6
Tulsina0509 <74660837+Tulsina0509@users.noreply.github.com>
2021-09-14 19:38:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+object=[];
+status="";
 video="";
 function preload(){
   video=createVideo('video.mp4');
@@ -13,12 +15,25 @@ image(video,0,0,480,380);
   if(status != "")
   {
     objectDetector.detect(video,gotResult);
+    for (i=0; 1< objects.length; i++) {
+      document.getElementById("status").innerHTML = "Staus : Objects Detected";
+        document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+objects.length";
+      
+      fill("#FF577F");
+      percent = floor(objects[i].confidence * 100);
+      text(objects[i].label + " " + percent + "%",objects[i].x + 15, objects[i].y +15);
+      noFill();
+      stoke("#00AF91");
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i],height);
+    }
+      
   }
   function gotResult(error, results) {
     if(error) {
       console.log(error);
     }
     console.log(results);
+    object=results;
 }
   function start () 
 {
